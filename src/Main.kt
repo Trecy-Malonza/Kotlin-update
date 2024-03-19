4131884b9b3dcc2632adf49affda1ddbfe280feb
Trecy-Malonza <trecymalonza@gmail.com>
2024-03-19 08:05:22 +0300

    added a document

---
+++ src/Main.kt
@@ -0,0 +1,148 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var num1 = 50
+    var num2 = 30
+    var sum = num1 + num2
+    println(sum)
+
+    var num3 = 34
+    var num4 = 234
+    var remainder = num3 % num4
+    println(remainder)
+
+    var a = 4
+    var b = 5
+    a = a + b
+    println(a)
+    var y = b++
+    println(y)
+    print(b)
+var result = 567
+    ++result
+    print(result)
+
+    var age = 56
+    --age
+    print(age)
+}
+
+fun main() {
+    name("Tracy")
+}
+
+fun name(word: String) {
+    println(word)
+}
+
+fun main() {
+    var num1 = 50
+    var num2 = 30
+    var sum = num1 + num2
+    println(sum)
+
+/*fun main(){
+    number(50,30)
+}
+
+fun number(num1: Int, num2: Int) {
+    var sum = num1 +num2
+    println(sum)
+}*/
+
+fun main(){
+    name("Tracy")
+}
+
+fun name(word:String) {
+    println(word)
+}
+
+/*fun main(){
+    number(67,78)
+
+}*/
+fun number(num3:Int , num5:Int): Int{
+    var sum = num3 + num5
+    println(sum)
+    return sum
+}
+
+/*fun main() {
+    word("Tracy",19)
+}
+
+fun word(name: String, age: Int):String {
+    var sentence = "$name $age"
+    println(sentence)
+    return sentence
+}*/
+
+/*fun main(){
+    val x = "My name is"
+    val name = "Adal"
+    println("$x $name")
+
+}*/
+
+/*fun main() {
+    val f = "expensive"
+    println(f.first())
+
+}*/
+
+/*fun main(){
+    name("Expensive")
+}
+
+fun name(f:String) {
+    println(f.first())
+    println(f[6])
+}*/
+
+/*fun main(){
+    sentence("Hybrid", 34
+    )
+    number("we are here", "it is us")
+    case("I love my friend")
+}
+fun sentence(name:String ,age:Int){
+    val name = "Hybrid"
+    var age = 34
+    var sentence = "$name is $age"
+    println(sentence)
+
+}
+fun number(message:String,message2:String){
+if(message.equals(message2)) {
+    println(true)
+} else {
+    println(false)
+}
+}
+
+fun case(x: String){
+    println(x.uppercase())
+    println(x.lowercase())
+}*/
+
+
+/*fun main(){
+    var name = " Brenda "
+    println(name.trimStart())
+    println(name.trimEnd())
+    println(name.trim())
+
+}
+
+fun main(){
+    var name = "principle"
+    
+}*/
+//fun main(){
+ //   takeString("Trecy")
+//}
+//fun takeString(name: String){
+ //   var length = name.length
+//    println(length)
+//}
